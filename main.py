7e4980eda85feed45a0882a9ce5c2748e8050f45
Mahmoud Khalid <mahmoudkhalid392@gmail.com>
2017-04-26 01:01:42 +0200

    correct train data with zero error

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 
-from imutils import contours
 import cv2
 import imutils
 from math import atan2, degrees
@@ -12,7 +11,7 @@ import numpy as np
 ANSWER_KEY1 = {0: 1, 1: 2, 2: 0, 3: 0, 4: 3,5:0,6:2,7:2,8:0,9:2,10:0,11:1,12:2,13:2,14:1}
 ANSWER_KEY2 = {0:0,1:3,2:1,3:2,4:1,5:3,6:2,7:3,8:1,9:3,10:2,11:3,12:3,13:1,14:2}
 ANSWER_KEY3 = {0:1,1:1,2:3,3:2,4:1,5:2,6:1,7:2,8:2,9:0,10:1,11:1,12:2,13:2,14:1}
-
+offset = 1500
 ####################################################################
 
 directory = "train/"
@@ -21,53 +20,21 @@ directory = "train/"
 
 kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
 
-#################### Get the last two circles ##################
+#################################################################
+#############################################################
+def gtc(p1):
 
-def Get_last_two_circles(image):
-
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    p = p1[offset:,:]
+    gray = cv2.cvtColor(p, cv2.COLOR_BGR2GRAY)
     blurred = cv2.GaussianBlur(gray, (5, 5), 0)
-    edged = cv2.Canny(blurred, 75, 200)
 
-    cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,
-                            cv2.CHAIN_APPROX_SIMPLE)
+    circles = cv2.HoughCircles(blurred, cv2.HOUGH_GRADIENT, 1, 20,
+                               param1=100, param2=30, minRadius=10, maxRadius=50)
 
-    cnts = cnts[0] if imutils.is_cv2() else cnts[1]
-
-    circles = []
-
-    # loop over the contours
-    for c in cnts:
-        # compute the bounding box of the contour, then use the
-        # bounding box to derive the aspect ratio
-        (x, y, w, h) = cv2.boundingRect(c)
-        ar = w / float(h)
-
-        # in order to label the contour as a question, region
-        # should be sufficiently wide, sufficiently tall, and
-        # have an aspect ratio approximately equal to 1
-        if w >= 20 and h >= 20 and ar >= 0.9 and ar <= 1.1:
-            circles.append(c)
-
-    circles = contours.sort_contours(circles, method="top-to-bottom")[0]
-    v = [circles[len(circles) - 1], circles[len(circles) - 2]]
-    cv2.drawContours(image, v, -1, 255, -1)
-    # cv2.imwrite("fff.png",image)
-    M = cv2.moments(v[0])
-    M1 = cv2.moments(v[1])
-    cX = int(M["m10"] / M["m00"])
-    cY = int(M["m01"] / M["m00"])
-    cX2 = int(M1["m10"] / M1["m00"])
-    cY2 = int(M1["m01"] / M1["m00"])
-
-    return cX,cX2,cY,cY2
-
-##################################################################
-
-###############################################################
-
-def rotate_image(cX,cX2,cY,cY2,image):
-
+    circles = np.round(circles[0, :]).astype("int")
+    c1 = circles[0]
+    c2 = circles[1]
+    cX, cX2, cY, cY2 = c1[0], c2[0], c1[1], c2[1]
     if cX < cX2:
         delta_x, delta_y = cX2 - cX, cY2 - cY
     else:
@@ -75,17 +42,30 @@ def rotate_image(cX,cX2,cY,cY2,image):
 
     rads = atan2(delta_y, delta_x)
     angle = degrees(rads)
-    if angle > -3 and angle < 3:
-        image = imutils.rotate(image, angle)
-    return image
+    image = imutils.rotate(p1,angle)
 
-############################ modify the list of circles  ###########################
+    p = p1[offset:, :]
+    gray = cv2.cvtColor(p, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+
+    circles = cv2.HoughCircles(blurred, cv2.HOUGH_GRADIENT, 1, 20,
+                               param1=100, param2=30, minRadius=10, maxRadius=50)
+
+    circles = np.round(circles[0, :]).astype("int")
+    c1 = circles[0]
+    c2 = circles[1]
+    cY2 = c2[1] + offset
+    return image,cY2
 
 
-def modify_this_list(circles):
 
-    c = sorted(circles, key=lambda tup: tup[0])
-    min_x = c[0][0]
+
+#########################################################
+############################ modify the list of circles  ##############
+
+
+def modify_this_list(circles,n):
+
     circles = sorted(circles, key=lambda tup: tup[1])
     questions = []
     work = [circles[0]]
@@ -104,6 +84,16 @@ def modify_this_list(circles):
     for o in questions:
         w = sorted(o, key=lambda tup: tup[0])
         sorted_questions.append(w)
+    if len(sorted_questions) > 15:
+        if n == 3:
+            sorted_questions = sorted_questions[:15]
+        elif n == 1:
+            sorted_questions.pop(0)
+    min_x = sorted_questions[0][0][0]
+
+    for j in sorted_questions:
+        if min_x > j[0][0]:
+            min_x = j[0][0]
 
     for q in range(0,len(sorted_questions),1):
         o = sorted_questions[q]
@@ -114,8 +104,6 @@ def modify_this_list(circles):
                 ,np.array([min_x+82,y,r]),np.array([min_x+123,y,r])]
 
             sorted_questions[q] = new_q
-    if len(sorted_questions) > 15:
-        sorted_questions = sorted_questions[:15]
 
     if len(sorted_questions) < 15:
         index = 0
@@ -150,15 +138,17 @@ def correct_this_page(p1,n):
     thresh = cv2.threshold(gray, 0, 255,
                            cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
     thresh =  cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
+
     circles = cv2.HoughCircles(blurred, cv2.HOUGH_GRADIENT, 1, 20,
                                param1=100, param2=30, minRadius=10, maxRadius=50)
+
     circles = np.round(circles[0, :]).astype("int")
 
-    sorted_questions = modify_this_list(circles)
+    sorted_questions = modify_this_list(circles,n)
     correct = 0
     number_of_question = 0
     for q in sorted_questions:
-        bubbled = (130,5)
+        bubbled = (110,5)
         number_of_circle = 0
         list_of_bubbled = []
         count = 0
@@ -169,8 +159,6 @@ def correct_this_page(p1,n):
 
             mask = cv2.bitwise_and(thresh, thresh, mask=mask)
             total = cv2.countNonZero(mask)
-            # if number_of_question==13:
-            #     print total
             if  total >= bubbled[0] or abs(bubbled[0] - total) < 20:
                 bubbled = (total, number_of_circle)
                 list_of_bubbled.append(bubbled)
@@ -199,10 +187,11 @@ def correct_this_page(p1,n):
 
 
 #############################  Main  ###############################
-
+directory1 = "images/"
 f = open("reuslt2.csv","w")
 f.write("FileName,Mark"+"\n")
 read_name_of_file = open("train.csv","r")
+
 for filename in read_name_of_file:
 
     filename= filename.split(",")
@@ -211,11 +200,8 @@ for filename in read_name_of_file:
     name = name.replace("\n", "")
     name = name.replace("\r", "")
     image = cv2.imread(name)
-
-    cX, cX2, cY, cY2 = Get_last_two_circles(image)
-    image = rotate_image(cX,cX2,cY,cY2,image)
-    image = image[cY2 - 770:cY2 - 80, 90:1100]
-
+    image,cY2 = gtc(image)
+    image = image[cY2 - 785:cY2 - 80, 90:1100]
     p1 = image[:, 0:350]
     p2 = image[:, 380:680]
     p3 = image[:, 720:]
@@ -224,6 +210,7 @@ for filename in read_name_of_file:
     correct3, p3 = correct_this_page(p3, 3)
     data = filename.replace("\n", "").replace("\r", "") + "," + str((correct1 + correct2 + correct3))
     f.write(data + "\n")
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
